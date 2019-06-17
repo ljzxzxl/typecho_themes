707924cb33dcda3c9433c3ba976de1c2aebcae65
周晓龙 <ljzxzxl@gmail.com>
2019-06-17 15:41:58 +0800

    顶栏抽屉文章分页数调整

--- Minimalism/header.php
+++ Minimalism/header.php
@@ -55,7 +55,7 @@
                 <ul>
                     <li class="toctree-l1">
                         <a class="reference internal" href="<?php $categories->permalink(); ?>"><?php $categories->name(); ?></a>
-                        <?php $this->widget('Widget_Archive@category-' . $categories->mid, 'pageSize=7&type=category', 'mid=' . $categories->mid)->to($posts); ?>
+                        <?php $this->widget('Widget_Archive@category-' . $categories->mid, 'pageSize=20&type=category', 'mid=' . $categories->mid)->to($posts); ?>
                         <ul>
                             <?php while ($posts->next()): ?>
                             <li class="toctree-l2"><a class="reference internal" href="<?php $posts->permalink(); ?>" title="<?php $posts->title(); ?>"><?php $posts->title(40); ?></a></li>
