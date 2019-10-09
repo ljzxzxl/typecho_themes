8b4ee3e15271b9b9641f5408d51fbc350df76a5c
周晓龙 <xiaolong.zhou@molbase.com>
2019-10-09 16:58:22 +0800

    Google统计更新

--- Minimalism/footer.php
+++ Minimalism/footer.php
@@ -103,12 +103,12 @@
     $(function(){ $(".post-content img").popImg(); });
 </script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-19926302-2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124162515-1"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'UA-19926302-2');
+    gtag('config', 'UA-124162515-1');
 </script>
 </body>
 </html>
